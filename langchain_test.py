062d59958a8ec41ae433411af89b77a37ecd83cb
Kim Jae Jun <rlawowns97@naver.com>
2025-04-04 01:29:04 +0900

    간단한 실행 예제 코드 작성 및 실행하여 확인

--- langchain_test.py
+++ langchain_test.py
@@ -1,7 +1,15 @@
+# 유투브 강의 : https://www.youtube.com/watch?v=VwHXDARgsdE
+
 import os
 from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
 
 load_dotenv() # load .env file
 
 API_KEY = os.environ['OPEN_AI_API_KEY']
-print(API_KEY)
+CHAT_GPT_MODEL = os.environ['OPEN_AI_MODEL']
+
+llm = ChatOpenAI(model=CHAT_GPT_MODEL, openai_api_key=API_KEY)
+
+print(llm.invoke("캐시슬라이드 설치했는데 리워드가 지급되지 않았어요. 확인해주세요."))
+
