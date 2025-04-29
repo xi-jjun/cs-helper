0e15d68024eee10f96e0bc3bdc23de2582867dc6
Kim Jae Jun <rlawowns97@naver.com>
2025-04-30 01:18:21 +0900

    marker_pdf 를 통한 PDF 파싱 진행 - 그러나 통짜 이미지는 파싱하지 못함

---
+++ pdf_parser/marker_pdf.py
@@ -0,0 +1,9 @@
+from marker.converters.pdf import PdfConverter
+from marker.models import create_model_dict
+from marker.output import text_from_rendered
+
+converter = PdfConverter(
+    artifact_dict=create_model_dict(),
+)
+rendered = converter("../pdf/test_catalog.pdf")
+text, _, images = text_from_rendered(rendered)
